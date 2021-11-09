f6fffcb5580c4c3172716ddd02499e554588cda5
PraveenTejDS <93979395+PraveenTejDS@users.noreply.github.com>
2021-11-09 15:36:15 +0530

    Add files via upload

---
+++ awslab2.py
@@ -0,0 +1,12 @@
+"""
+Your module description
+"""
+name="Praveen Tej"
+print('My name is ',name)
+age=23
+print('My age is ',age)
+print ('my name is ',name,'my age is ',age)
+salary=50000
+print('my salary is',salary)
+print('my name is ',name, 'my age is ', age,'my salary is ',salary)
+print("my name is ",name,".","\nmy age is ",age,"\nmy salary is ",salary,".")
---
+++ whileloop1.py
@@ -0,0 +1,5 @@
+i=5
+for n in range(1,11,1):
+while(i<=50):
+    print(i)
+    i=i*n
